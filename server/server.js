f08abc85834e32d7d5b9291745f4dce4c1c27656
tkozemzak <timkozemzak@gmail.com>
2021-04-23 12:06:47 -0700

    set up mongoose for mongodb. create schema

--- server/server.js
+++ server/server.js
@@ -1,5 +1,17 @@
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+const Document = require("./Document");
+
+mongoose.connect("mongodb://localhost/docs-clone-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const defaultValue = "";
+
 const io = require("socket.io")(process.env.PORT, {
   cors: {
     origin: process.env.CLIENT_ORIGIN,
@@ -8,8 +20,22 @@ const io = require("socket.io")(process.env.PORT, {
 });
 
 io.on("connection", (socket) => {
-  socket.on("send-changes", (delta) => {
-    socket.broadcast.emit("receive-changes", delta);
+  socket.on("get-document", (documentId) => {
+    const data = "";
+    socket.join(documentId);
+    socket.emit("load-document", data);
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
   });
+
   console.log("Connected");
 });
+
+const findOrCreateDocument = async (id) => {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+};
