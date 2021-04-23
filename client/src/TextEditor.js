dcc76fad1199d10f593e6eb7db8c5bb937735b3f
tkozemzak <timkozemzak@gmail.com>
2021-04-23 11:49:59 -0700

    configure client to receive doc from server. disable text editing until document is received from server

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,6 +2,7 @@ import { useState, useEffect, useCallback } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,6 +17,7 @@ const TOOLBAR_OPTIONS = [
 ];
 
 const TextEditor = () => {
+  const { id: documentId } = useParams();
   const [socket, setSocket] = useState();
   const [quill, setQuill] = useState();
 
@@ -52,6 +54,8 @@ const TextEditor = () => {
       theme: "snow",
       modules: { toolbar: TOOLBAR_OPTIONS },
     });
+    q.disable();
+    q.setText("Loading...");
     setQuill(q);
   }, []);
 
@@ -67,6 +71,17 @@ const TextEditor = () => {
       socket.off("receive-changes", handler);
     };
   }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    socket.once("load-document", (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
+
+    socket.emit("get-document", documentId);
+  }, [socket, quill, documentId]);
 
   return <div className="container" ref={wrapperRef}></div>;
 };
