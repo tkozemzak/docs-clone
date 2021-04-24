b1b1f864bf5bb20decbd64079ead4e80cddd892e
tkozemzak <timkozemzak@gmail.com>
2021-04-23 19:23:12 -0700

    fix server connection issue. Update mongo config

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,9 +1,10 @@
-import { useState, useEffect, useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import { io } from "socket.io-client";
 import { useParams } from "react-router-dom";
 
+const SAVE_INTERVAL_MS = 2000;
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
   [{ font: [] }],
@@ -16,63 +17,19 @@ const TOOLBAR_OPTIONS = [
   ["clean"],
 ];
 
-const SAVE_INTERVAL_MS = 2000;
-
-const TextEditor = () => {
+export default function TextEditor() {
   const { id: documentId } = useParams();
   const [socket, setSocket] = useState();
   const [quill, setQuill] = useState();
 
   useEffect(() => {
     const s = io(process.env.REACT_APP_SERVER_URL);
-
     setSocket(s);
 
     return () => {
       s.disconnect();
     };
   }, []);
-
-  useEffect(() => {
-    if (socket == null || quill == null) return;
-    const handler = (delta, oldDelta, source) => {
-      if (source !== "user") return;
-      socket.emit("send-changes", delta);
-    };
-
-    quill.on("text-change", handler);
-
-    return () => {
-      quill.off("text-change");
-    };
-  }, [socket, quill]);
-
-  const wrapperRef = useCallback((wrapper) => {
-    if (wrapper == null) return;
-    wrapper.innerHTML = "";
-    const editor = document.createElement("div");
-    wrapper.append(editor);
-    const q = new Quill(editor, {
-      theme: "snow",
-      modules: { toolbar: TOOLBAR_OPTIONS },
-    });
-    q.disable();
-    q.setText("Loading...");
-    setQuill(q);
-  }, []);
-
-  useEffect(() => {
-    if (socket == null || quill == null) return;
-    const handler = (delta) => {
-      quill.updateContents(delta);
-    };
-
-    socket.on("receive-changes", handler);
-
-    return () => {
-      socket.off("receive-changes", handler);
-    };
-  }, [socket, quill]);
 
   useEffect(() => {
     if (socket == null || quill == null) return;
@@ -91,12 +48,52 @@ const TextEditor = () => {
     const interval = setInterval(() => {
       socket.emit("save-document", quill.getContents());
     }, SAVE_INTERVAL_MS);
+
     return () => {
       clearInterval(interval);
     };
   }, [socket, quill]);
 
-  return <div className="container" ref={wrapperRef}></div>;
-};
+  useEffect(() => {
+    if (socket == null || quill == null) return;
 
-export default TextEditor;
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+    socket.on("receive-changes", handler);
+
+    return () => {
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
+
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    q.disable();
+    q.setText("Loading...");
+    setQuill(q);
+  }, []);
+  return <div className="container" ref={wrapperRef}></div>;
+}
--- server/server.js
+++ server/server.js
@@ -3,14 +3,12 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const Document = require("./Document");
 
-mongoose.connect("mongodb://localhost/docs-clone-db", {
+mongoose.connect("mongodb://localhost/google-docs-clone", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
 });
-
-const defaultValue = "";
 
 const io = require("socket.io")(process.env.PORT, {
   cors: {
@@ -19,26 +17,29 @@ const io = require("socket.io")(process.env.PORT, {
   },
 });
 
+const defaultValue = "";
+
 io.on("connection", (socket) => {
+  console.log("connected");
   socket.on("get-document", async (documentId) => {
     const document = await findOrCreateDocument(documentId);
     socket.join(documentId);
     socket.emit("load-document", document.data);
+
     socket.on("send-changes", (delta) => {
       socket.broadcast.to(documentId).emit("receive-changes", delta);
     });
-  });
 
-  socket.on("save-document", async (data) => {
-    await Document.findByIdAndUpdate(documentId, { data });
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
   });
-  console.log("Connected");
 });
 
-const findOrCreateDocument = async (id) => {
+async function findOrCreateDocument(id) {
   if (id == null) return;
 
   const document = await Document.findById(id);
   if (document) return document;
   return await Document.create({ _id: id, data: defaultValue });
-};
+}
